3e6da365512418b2390e60dcfa91e87ed4f473e6
Harish Ganesan <harishganesan96@gmail.com>
2019-04-23 14:46:05 +0530

    Added Kruskal's algorithm

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CPPFLAGS= -std=c++11
 
 .PHONY:all testing
-all: problem1 problem3 problem5a
+all: problem1 problem3 problem5a problem5b
 
 testing: random_graph
 
@@ -12,6 +12,9 @@ problem3: problem3.o sud_graph.o sd_graph.o vertex.o
 	g++ ${CPPFLAGS} -g $^ -o $@
 
 problem5a: problem5a.o sud_graph.o sd_graph.o vertex.o
+	g++ ${CPPFLAGS} -g $^ -o $@
+
+problem5b: problem5b.o sud_graph.o sd_graph.o vertex.o
 	g++ ${CPPFLAGS} -g $^ -o $@
 
 sud_graph: sud_graph.o sd_graph.o vertex.o
@@ -29,4 +32,7 @@ random_graph: random_graph.o sud_graph.o sd_graph.o vertex.o
 clean:
 	@rm -f *.o
 	@rm -f problem1
+	@rm -f problem3
+	@rm -f problem5a
+	@rm -f problem5b
 	@rm -f random_graph
--- problem5b.cpp
+++ problem5b.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "random_graph.h"
 #include "sud_graph.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -30,7 +31,6 @@ void usage( char* arg )
 	cout<<arg<<" <Size of graph> <Number of edges> <type> <source>\n";
 	cout<<" Size of the graph - Integer\n";
 	cout<<" Number of edges - Integer\n";
-	cout<<" Source node - Integer\n";
 }
 
 /**
@@ -41,10 +41,10 @@ class d_set {
 	vector<int> rep;
 	vector<int> rank;
 public:
-	int union( int first, int second );
+	int set_union( int first, int second );
 	int find( int index );
 	d_set( int n );
-}
+};
 
 /**
  * @func union
@@ -53,9 +53,9 @@ public:
  * @param first First representative
  * @param second Second representative
  *
- * @return 0 on success, -1 on error
+ * @return 1 if second becomes the child, 2 if first becomes the child, -1 on error
  */
-int d_set::union( int first, int second )
+int d_set::set_union( int first, int second )
 {
 	if( first < 0 || first >= rep.size() )
 		return -1;
@@ -68,14 +68,17 @@ int d_set::union( int first, int second )
 
 	if( rank[first] > rank[second] ){
 		rep[second] = first;
+		return 1;
 	} else if( rank[first] < rank[second] ){
 		rep[first] = second;
+		return 2;
 	} else{
-		rep[first] = second;
+		rep[second] = first;
 		rank[first]++;
+		return 1;
 	}
 
-	return 0;
+	//return 0;
 }
 
 /**
@@ -128,11 +131,60 @@ d_set::d_set( int n )
 		rep[i] = i;
 }
 
+typedef pair<int,int> sides;
+typedef pair<sides,int> edge;
 
-int kruskal_mst( sud_graph& graph, vector<int>& arr )
+bool edge_comp( edge& first, edge& second )
+{
+	return ( first.second > second.second );
+}
+
+int kruskal_mst( sud_graph& graph, vector<pair<int,int>>& arr )
 {
 	int size = graph.get_graph_size();
 	d_set set(size);
+	vector< edge > elist; 
+	arr.clear();
+	int taken=0;
+	arr.reserve( size );
+	arr.resize( size, pair<int,int>(-1,0) );
+
+	for( int i=0; i<size; i++ ){
+		vertex v = graph.get_vertex( i );
+		auto begin = v.cbegin(), end = v.cend();
+
+		for(; begin != end; begin++ ){
+			if( begin->first <= i ) continue;
+			sides t1( i, begin->first );
+			edge t2( t1, begin->second );
+			elist.push_back( t2 );
+		}
+	}
+
+	sort( elist.begin(), elist.end(), edge_comp );
+	taken = 1;
+	while( taken <= size - 1 ){
+		cout<<elist.size()<<endl;
+		edge e = elist.back();
+		sides s = e.first;
+		int rep1,rep2;
+
+		elist.pop_back();
+		rep1 = set.find( s.first );
+		rep2 = set.find( s.second );
+
+		if( rep1 != rep2 ){
+			int ret = set.set_union( rep1, rep2 );
+		       	if( ret == 1 )
+				arr[s.second] = pair<int,int>( s.first, e.second );	
+			else if( ret == 2 )
+				arr[s.first] = pair<int,int>( s.second, e.second );
+			taken++;
+		}
+	}	
+	
+	return 0;
+}	
 
 
 int main( int argc, char *argv[] )
@@ -143,9 +195,13 @@ int main( int argc, char *argv[] )
 	int n,edges;
 	int source;
 	string type;
-	vector<int> arr;
-
-	if( argc != 4 ){
+	vector<pair<int,int>> arr;
+	FILE *fp = fopen("mst.dot", "w" );
+	if( !fp ){
+		exit(-1);
+	}
+		
+	if( argc != 3 ){
 		usage(argv[0]);
 		exit(-1);
 	}
@@ -158,18 +214,28 @@ int main( int argc, char *argv[] )
 		usage(argv[0]);
 		exit(-1);
 	}
-	
+
+	ret = generate_random_connected_graph( udgraph, n, edges );	
 	//ret = dijkstra_shortest_path( udgraph, source, arr );
 	ret = kruskal_mst( udgraph, arr );
-	ret = udgraph.print_graph_graphviz( "problem5aud.dot" );
-	system( " dot -Tsvg -O problem5aud.dot " );
+	ret = udgraph.print_graph_graphviz( "problem5b.dot" );
+	system( " dot -Tsvg -O problem5b.dot " );
+	
 	
 	int size = arr.size();
-	for( int i=0; i< size; i++ )
-		if( arr[i] )
-			cout<<i<<"->"<<arr[i]<<endl;
-	cout<<"\n";
+	fprintf(fp,"digraph{\n");
+	for( int i=0; i< size; i++ ){
+		if( arr[i].first != -1 )
+			//cout<<i<<"->"<<arr[i].first<<" [ label= "<<arr[i].second<<" ]\n";
+			fprintf(fp,"%d->%d [ label=\"%d\",dir=none ]\n",i,arr[i].first,arr[i].second);
+	}
+	fprintf(fp,"}\n");
+	fclose(fp);
+	system( " dot -Tsvg -O mst.dot" );
 
+
+	//cout<<"\n";
+	
 	return 0;
 }
 
